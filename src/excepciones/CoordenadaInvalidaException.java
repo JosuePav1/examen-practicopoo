8b8bb139e154403b7bf786b9b521845385368835
Erick Gavilanes <egavilanesh1@est.ups.edu.ec>
2026-06-05 21:34:46 -0500

    Delete src directory
    delete

--- src/excepciones/CoordenadaInvalidaException.java
+++
@@ -1,7 +0,0 @@
-package excepciones;
-
-public class CoordenadaInvalidaException extends Exception {
-    public CoordenadaInvalidaException() {
-        super("Coordenada inválida. Usa formato como A5 o M A5");
-    }
-}
